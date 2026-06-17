5189749bbd7eac28be634d551406bb3573336add
spiderman <spiderman@xhift.co.kr>
2026-06-18 06:53:23 +0900

    refectored

--- completion/src/main/java/com/example/demo/config/SecurityConfig.java
+++ completion/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -7,7 +7,6 @@ import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
